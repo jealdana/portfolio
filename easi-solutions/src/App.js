bc294ec0accfc7ed1dac0b9ca05eab6f2586e449
jealdana <aldana.sigona@gmail.com>
2020-01-01 22:33:55 +0200

    Creating routing module

--- easi-solutions/src/App.js
+++ easi-solutions/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import Assets from './components/Assets.js'
+import Graphs from './components/Graphs.js'
 // Lessons learned: state hooks, handlers, preventDefault function, filter function, map function 
 
 import {
@@ -13,47 +14,36 @@ import {
 function BasicExample() {
   return (
     <Router>
-      <div className='navbar navbar-light bg-light'>
-        <ul>
-          <li className='nav-item'>
-            <Link to="/">Home</Link>
+      <nav className='navbar navbar-expand-lg'>
+        <ul className='navbar-nav'>
+          <li className='nav-item mr-3'>
+            <Link to="/">Graphs</Link>
           </li>
-          <li className='nav-item'>
+          <li className='nav-item mr-3'>
             <Link to="/about">About</Link>
           </li>
-          <li className='nav-item'>
+          <li className='nav-item mr-3'>
             <Link to="/dashboard">Dashboard</Link>
           </li>
         </ul>
-
-        <hr />
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Graphs />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 // You can think of these components as "pages"
 // in your app.
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
 
 function About() {
   return (
---
+++ easi-solutions/src/components/Graphs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Graphs() {
+  return (
+    <div>
+      <h2>Graphs</h2>
+    </div>
+  );
+}
